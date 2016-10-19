c423523612f0ae278471e65a5dc7ce1358e7d4a0
Hugo Iván Vázquez Palacios <hugo.vazquez@previta.mx>
2016-10-19 18:31:14 -0500

    server texto modular

---
+++ config/config.js
@@ -0,0 +1,3 @@
+module.export = {
+    'IP' : process.env.IP || '127.0.0.1', 'PORT' : process.env.PORT || 3000
+};
--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-//cargando el modulo http
+//Cargando el modulo http
 var http = require('http');
+//Cargando la libreria colors
+var colors = require('colors');
+//Generando un tema
+colors.setTheme({
+    silly: 'rainbow',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    info: 'green',
+    data: 'grey',
+    help: 'cyan',
+    warn: ['yellow', 'bgWhite'],
+    debug: 'blue',
+    error: 'red',
+    achivement: 'rainbow'
+});
 
-//creando el servidor
-var server = http.createServer(function(req, res){
+//Obteniendo configuraciones
+var IP = 'localhost';
+var PORT = 3000;
+//Creando el servidor
+var server = http.createServer(function (req, res) {
     res.writeHead(
         200,
         {
-            'Content-Type' : 'text/plain',
-            'Server' : 'Buho@0.0.0'
+            'Content-Type': 'text/plain',
+            'Server': 'Buho@0.0.0'
         }
     );
     res.write('Hola desde el server');
@@ -15,6 +34,6 @@ var server = http.createServer(function(req, res){
 });
 
 //Poniendo a escuchar el server
-server.listen(3000, 'localhost', function(){
-    console.log('> Server escuchando en: http://localhost:3000/ ...' );
+server.listen(PORT, IP, function () {
+    console.log("> Server escuchando en" + `http://${IP}:${PORT}`.info);
 });
